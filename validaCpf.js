a843c9e5fedc8a25a5d30d90b2849c144d20ef6b
gabriel goncalo <gabriel.goncalo.705@ufrn.edu.br>
2024-09-15 16:07:02 -0300

    valida cpf quase completo

--- validaCpf.js
+++ validaCpf.js
@@ -1,4 +1,39 @@
-let cpf = `705.989.232-23`;
+function ValidaCPF(cpfEnviado){
+    Object.defineProperty(this,'cpfLimpo', {
+        enumerable: true,
+        get: function(){
+            return cpfEnviado.replace(/\D+/g, '');
+        },
+    });
 
-let cpfLimpo = cpf.replace(/\D+/g,``);
 
+}
+
+ValidaCPF.prototype.valida = function (){
+    if(typeof this.cpfLimpo === 'undefined') return false;
+    if(this.cpfLimpo.length !== 11) return false;
+    
+    const cpfParcial = this.cpfLimpo.slice(0, -2);
+    const digito1 = this.criaDigito(cpfParcial);
+    const digito2 = this.criaDigito(cpfParcial + digito1);
+    const novoCpf = cpfParcial + digito1 + digito2;
+
+    return novoCpf === this.cpfLimpo;
+}
+
+ValidaCPF.prototype.criaDigito = function(cpfParcial){
+    const cpfArray = Array.from(cpfParcial);
+    let regressivo = cpfArray.length + 1;
+    const total = cpfArray.reduce((ac, val) => {
+        ac += (regressivo * Number(val));
+        regressivo--;
+        return ac;
+    }, 0);
+
+    const digito = 11 - (total % 11);
+    //Se o digito for maior que 9, retorna zero, ao contrario retorna o digito
+    return digito > 9 ? '0' : String(digito);
+    
+};
+const cpf = new ValidaCPF('705.484.450.52');
+console.log(cpf.valida());
