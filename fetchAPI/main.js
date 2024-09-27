20269c4d463355d0adf301f72f57c611fb72d033
gabriel goncalo <gabriel.goncalo.705@ufrn.edu.br>
2024-09-27 15:35:32 -0300

    fetch api

---
+++ fetchAPI/main.js
@@ -0,0 +1,54 @@
+document.addEventListener('click', e => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+    if (tag === 'a'){
+        e.preventDefault();
+        carregaPagina(el);
+    }
+});
+
+function carregaPagina(el){
+    const href = el.getAttribute('href');
+
+    fetch(href)
+        .then(response => {
+            if(response.status != 200) throw new Error('ERRO 404 NOSSO');
+            return response.text();
+        })
+        .then(html => carregaResultado(html))
+        .catch(e => console.log(e));
+}
+
+function carregaResultado(response){
+    const resultado = document.querySelector('.resultado');
+    resultado.innerHTML= response;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*fetch('pagina1.html')
+    .then(resposta => {
+        if(resposta.status !== 200 ) throw new Error('ERRO 404 NOSSO');
+        return resposta.text();
+
+    })
+    .then(html => console.log(html))
+    .catch(e => console.log(e));*/
