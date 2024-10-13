48b4a4b8c6e71187f117541adac640976c2dad4f
gabriel goncalo <gabriel.goncalo.705@ufrn.edu.br>
2024-10-12 23:51:45 -0300

    varias url

--- NODE/express/server.js
+++ NODE/express/server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) =>{
     res.send('Hello world!');
 });
 
+app.get('/contato', (req, res) => {
+    res.send('Bem vindo a aba contato.')
+});
+
 app.listen(3000, ()=>{
     console.log('Acessar: http://localhost');
     console.log('Servidor executando na porta 3000');
